032422e2f9538d8c69b8f0447aea0df065197890
21010573DatNT <21010573@st.phenikaa-uni.edu.vn>
2025-11-19 18:59:28 +0700

    them nut cuon trang

--- TMDT-FE/src/Layouts/index.jsx
+++ TMDT-FE/src/Layouts/index.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Componets/Header';
 import Footer from '../Componets/Footer';
+import ScrollToTopButton from '../Componets/ScrollToTopButton';
 
 function Layout() {
-    return(
+    return (
         <>
             <Header />
             <main>
                 <Outlet />
             </main>
             <Footer />
+            <ScrollToTopButton />
         </>
     )
 }
